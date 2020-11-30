da767ff6faa683e3d8b3c863b5f60c38e8ecc72f
lucas <lucasx2002@hotmail.com>
2020-11-30 14:36:32 -0400

    fin cpp

--- prj_lf_c/main.cpp
+++ prj_lf_c/main.cpp
@@ -1,24 +1,12 @@
-<ui version="4.0">
-        <class>MainWindow</class>
-        <widget class="QMainWindow" name="MainWindow" >
-        <property name="geometry" >
-        <rect>
-        <x>0</x>
-        <y>0</y>
-        <width>400</width>
-        <height>300</height>
-        </rect>
-        </property>
-        <property name="windowTitle" >
-        <string>MainWindow</string>
-        </property>
-        <widget class="QMenuBar" name="menuBar" />
-        <widget class="QToolBar" name="mainToolBar" />
-        <widget class="QWidget" name="centralWidget" />
-        <widget class="QStatusBar" name="statusBar" />
-        </widget>
-        <layoutDefault spacing="6" margin="11" />
-        <pixmapfunction></pixmapfunction>
-        <resources/>
-        <connections/>
-        </ui>
+#include<QApplication>
+
+#include"client.h"
+
+int main(int argc, char *argv[]);
+{
+    QApplication app(argc, argv);
+    QGuiApplication::setApplicationDisplayName(Client::tr("Local Fortune Client"));
+    Client client;
+    client.show();
+    return app.exec();
+}
